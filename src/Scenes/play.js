499a2a6267d960c3366093569ded6eecee0f3e11
mclick <mclick@ucsc.edu>
2021-07-04 09:37:43 -0700

    i made some changes but by the time i commited them i forgot what they were

--- src/Scenes/play.js
+++ src/Scenes/play.js
@@ -10,15 +10,16 @@ class Play extends Phaser.Scene {
     create(){
         this.gameOver=false;
         this.backround = this.add.tileSprite(0, 0, 1280, 480, 'tempBackround').setOrigin(0, 0);
+        //player
         this.turkey = new Turkey(this, borderPadding+borderUISize, game.config.height/2, 'turkey').setOrigin(0.5, 0);
-
-        this.stick1= new Stick(this, borderPadding+borderUISize, game.config.height/2+5, 'stick', 0).setOrigin(0, 0);
+        //ground objects
+        this.stick1= new Stick(this, borderPadding+borderUISize, game.config.height/2+10, 'stick', 0).setOrigin(0, 0);
         this.stick2= new Stick(this, game.config.width * (2/6), game.config.height *(2/6), 'stick', 0).setOrigin(0, 0);
         this.stick3= new Stick(this, game.config.width * (3/6), game.config.height *(3/6), 'stick',0).setOrigin(0, 0);
         this.stick4= new Stick(this, game.config.width * (4/6), game.config.height *(4/6), 'stick',0).setOrigin(0, 0);
         this.stick5= new Stick(this, game.config.width * (5/6), game.config.height *(5/6), 'stick',0).setOrigin(0, 0);
-        this.stick6= new Stick(this, game.config.width * (6/6), game.config.height *(6/6), 'stick',0).setOrigin(0, 0);
-        this.stick7= new Stick(this, game.config.width /6, game.config.height/6, 'stick',0).setOrigin(0,0);
+        this.stick6= new Stick(this, game.config.width * (6/6), game.config.height *(6/6)-20, 'stick',0).setOrigin(0, 0);
+        this.stick7= new Stick(this, game.config.width /6, game.config.height*3/6, 'stick',0).setOrigin(0,0);
 
         keyJump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
