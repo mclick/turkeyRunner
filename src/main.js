a4ce4210c4f73a42bd045ba934ba1adae81ae206
Jordan Fickel <jfickel@ucsc.edu>
2021-07-06 12:00:16 -0700

    comment block in main, changed web color

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+/*
+    game title:         Turkey Strut 2021
+    completed:          July 6, 2021
+        
+    TEAM:
+        Matthew Click:      main programming/gameplay feel
+        Jordan Fickel:      music and some art
+        Stanley Caldwell:   lead art/visual design
+
+    Creative Tilt:
+    Jordan:     Made a 'lofi beat' for the first time. Autotuned 3 different turkey sounds and 
+                programmatically chose which one to use based on what portion of the song is playing
+                (check the playCall method in turkey.js). 
+                Drew a squirrel for the first time.
+    Matthew:
+    Stanley: 
+*/
+
+
 let config = {
     type: Phaser.AUTO,
     width: 1280,
@@ -15,3 +34,6 @@ let speedConst = 3;
 let accelConst = .125/60;
 
 let keyJump;
+
+
+
